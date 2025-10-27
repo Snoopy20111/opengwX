297f3bdaaf11a71db6c9da7bb6248467a5683694
Snoopy20111 <snoopy20111@gmail.com>
2025-10-26 20:28:23 -0500

    Step 4 (DEBUG COMPILES!) (Linker Error fixing)
    Show 'em the floor
    Make you say more more more
    
    [Only remaining linker error is a double definition in my libraries...?]

--- stdafx.cpp
+++ stdafx.cpp
@@ -6,3 +6,13 @@
 
 // TODO: reference any additional headers you need in STDAFX.H
 // and not in this file
+
+// Seems like a good enough place to put in solution-wide hacks, right?
+
+// stub for CxxFrameHandler3 errors, because neither RXDK nor NXDK support exceptions
+extern "C" void cdecl __CxxFrameHandler3() {}
+
+// Broken features in RXDK:
+// std::atomic
+// std::mutex
+// std::shared_ptr
